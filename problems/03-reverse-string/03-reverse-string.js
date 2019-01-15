22de228e08ceab3e64b03de46479951c4f647d12
Michael R. Roark <michaelrroark@gmail.com>
2019-01-15 14:56:17 -0500

    Finished 03. Updated manifest.

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -2,5 +2,14 @@
 Define function: reverseStr(someStr)
 Reverse and return the String. */
 function reverseStr(someStr) {
-  
+  let result = '';
+  let manipulatedString = someStr;
+
+  while (manipulatedString.length > 0) {
+    result = result + manipulatedString.slice(manipulatedString.length-1, manipulatedString.length);
+    manipulatedString = manipulatedString.slice(0,manipulatedString.length-1);
+  }
+
+  return result;
 }
+console.log(reverseStr('hello'));
