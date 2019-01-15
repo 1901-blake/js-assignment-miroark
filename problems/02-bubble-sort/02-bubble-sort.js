994b57291851b5d9dbd73a953a52ff26da6889dc
Michael R. Roark <michaelrroark@gmail.com>
2019-01-15 11:19:45 -0500

    Solved problem 2, started on problem 7 and created a manifest to denote as such, mostly for my own organization.

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -2,6 +2,21 @@
 Define function: bubbleSort(numArray)
 Use the bubble sort algorithm to sort the array.
 Return the sorted array. */
+
 function bubbleSort(numArray) {
-  
-}
+  for (let i = 0; i < numArray.length; i++){
+    for (let j = 0; j < numArray.length; j++){
+      if (numArray[i] < numArray[j]){
+        let temp = numArray[i];
+        numArray[i] = numArray[j];
+        numArray[j] = temp;
+      }
+    }
+  }
+}// end bubbleSort
+
+let array = [11,53,3,82,22,4,8,1,9,0,5,3,8,2,36,8,6,5732,82,47,9,4,2643,274];
+
+bubbleSort(array);
+
+console.log(array);
--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,17 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
+
 function isPalindrome(someStr) {
-  
+
+
+  return true;
 }
+
+let pally = "tacocat";
+let noPally = "tubacat";
+let maybePally = "taco cat";
+
+console.log(isPalindrome(pally));
+console.log(isPalindrome(noPally));
+console.log(isPalindrome(maybePally));
