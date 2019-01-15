3aa0b379edce1d9afccf521ef0e771e9e6110039
Michael R. Roark <michaelrroark@gmail.com>
2019-01-15 12:47:45 -0500

    Might have finished 07. Need to check if spaces should count in the string or not. If spaces matter, then it's done. If ignoring spaces then more work needs to be done.

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -3,14 +3,23 @@ Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 
 function isPalindrome(someStr) {
-
+  let i = 0;
+  let j = someStr.length-1;
+  while (i < j){
+    console.log(`${someStr.charAt(i)} == ${someStr.charAt(j)}`);
+    if (someStr.charAt(i) != someStr.charAt(j)) {
+      return false;
+    }
+    i++;
+    j--;
+  }
 
   return true;
 }
 
 let pally = "tacocat";
 let noPally = "tubacat";
-let maybePally = "taco cat";
+let maybePally = "taco cat"; //question is, should I include spaces in the pallindrome or not?
 
 console.log(isPalindrome(pally));
 console.log(isPalindrome(noPally));
