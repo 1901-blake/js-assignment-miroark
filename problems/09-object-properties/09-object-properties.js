a5f15b0eb20319333e1e4a23df7c4137b58fd046
Michael R. Roark <michaelrroark@gmail.com>
2019-01-16 10:37:21 -0500

    Finished indenting nested objects functionailty for 09. Working on Array handling.

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,19 +1,20 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
-function objectProperties(someObj) {
+function objectProperties(someObj, indents = 0) {
 
   for (item in someObj){
     if (someObj[item].constructor === Object){
       //Recurseively call objectProperties. Who knows how many objects are nested?
-      console.log(`Key: ${item}`);
-      console.log(`Value:`);
-      objectProperties(someObj[item]);
-    }//end of object handling
+      console.log(`${" ".repeat(indents)}Key: ${item}`);
+      console.log(`${" ".repeat(indents)}Value:{`);
+      objectProperties(someObj[item], indents+1);
+      console.log(`${" ".repeat(indents)}}`);
+    }//end of object handling. COMPLETE
 
     else if (someObj[item].constructor === Array) {
       //Iterate through the array, and display their values.
-      console.log(`Key: ${item}`);
+      console.log(`${" ".repeat(indents)}Key: ${item}`);
 
       //assemble string with contents in the array.
       let arrayContents = '[';
@@ -23,15 +24,15 @@ function objectProperties(someObj) {
       arrayContents = arrayContents.substring(0, arrayContents.length - 2);
       arrayContents += ']';
 
-      console.log(`Value: ${arrayContents}
+      console.log(`${" ".repeat(indents)}Value: ${arrayContents}
         `);
     }//end of array handling. Needs to be able to hand more arrays, or more objects.
 
     else {
-      console.log(`Key: ${item}`);
-      console.log(`Value: ${someObj[item]}
+      console.log(`${" ".repeat(indents)}Key: ${item}`);
+      console.log(`${" ".repeat(indents)}Value: ${someObj[item]}
         `);
-    }//end of primitives handling.
+    }//end of primitives handling. COMPLETE
   }//end of for (x in y)
 }//end of objectProperties
 
@@ -39,10 +40,16 @@ let testObj = {
   category : 'Fruit',
   object : {
     name : 'Banana',
-    Rating : 7,
-    Prices : [0.99,0.59,1.25]
+    rating : 7,
+    prices : [0.99,0.59,1.25],
+    randomObject : {
+      favoriteColor : 'Green',
+      why : 'Just Because'
+    }
   },
-  Orders : ['Tampa, FL', 'New Albany, IN', 'Newark, DE', 'Fresno, CA']
+  orders : ['Tampa, FL', 'New Albany, IN', 'Newark, DE', 'Fresno, CA'],
+  arrayOfObjects : [{name : 1, area : 'left'}, {name : 2, area:'right'}],
+  arrayOfArrays : [[1,2,3],[2,3,4],[4,5,6]]
 }
 
 objectProperties(testObj);
