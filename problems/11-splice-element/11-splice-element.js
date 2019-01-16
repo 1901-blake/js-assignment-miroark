8764dcdc7076ccc0f991119cdc3b55e64f4a9700
Michael R. Roark <michaelrroark@gmail.com>
2019-01-16 10:27:47 -0500

    Finished 11. Updated Manifest.

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -3,5 +3,8 @@ Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
 function spliceElement(someArr, index) {
-  
+//Assuming that at [index] we want to remove one item.
+  return someArr.splice(index, 1);
 }
+testArray = [1,2,3,4,5];
+console.log(`Removed ${spliceElement(testArray, 2)} from ${testArray}`);
